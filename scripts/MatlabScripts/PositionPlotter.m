b58e7b1562428b6946cfdfce5e399e25921f6dfa
Carlos <ceduardosa4@gmail.com>
2024-01-28 00:00:13 +0000

    updated matlab plotter script

--- scripts/MatlabScripts/PositionPlotter.m
+++ scripts/MatlabScripts/PositionPlotter.m
@@ -1,21 +1,27 @@
+clear all
+
 data_s = yaml.loadFile("test.yaml");
+
+%% Turn struct of struct to struct of arrays
+
+datafields = fieldnames(data_s);
+for k=1:numel(datafields)
+    data_smat.(datafields{k}) = cell2mat(data_s.(datafields{k}));
+end
+clear data_s datafields k
+
 %%
-WheelSpeedBL = cell2mat(data_s.WheelSpeedBL);
-WheelSpeedBR = cell2mat(data_s.WheelSpeedBR);
-WheelSpeedFL = cell2mat(data_s.WheelSpeedFL);
-WheelSpeedFR = cell2mat(data_s.WheelSpeedFR);
 
+subplot(4,1,1)
+plot(data_smat.VehiclePositionX,data_smat.VehiclePositionY)
 
-subplot(4,1,1);
-plot(WheelSpeedFR)
+subplot(4,1,2)
+plot(data_smat.CurrentTime,data_smat.VehiclePositionX)
 
-subplot(4,1,2);
-plot(WheelSpeedFL)
+subplot(4,1,3)
+plot(data_smat.CurrentTime,data_smat.VehiclePositionY)
 
-
-subplot(4,1,3);
-plot(WheelSpeedBL)
-
-
-subplot(4,1,4);
-plot(WheelSpeedBR)
+subplot(4,1,4)
+vehSpeedKph = data_smat.VehicleSpeed * 3.6;
+plot(data_smat.CurrentTime,vehSpeedKph)
+clear vehSpeedKph
